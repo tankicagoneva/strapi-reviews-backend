5a9adc0cf589c6773edfcc2f48e058817e91b7fe
tankicagoneva <tankicaspasev@gmail.com>
2024-01-09 18:57:43 +0000

    refactor: delete unused files and dependencies

--- backend/.strapi/client/app.js
+++ backend/.strapi/client/app.js
@@ -6,7 +6,6 @@ import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
 import graphql from "@strapi/plugin-graphql/strapi-admin";
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
-import vercelDeploy from "strapi-plugin-vercel-deploy/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 renderAdmin(document.getElementById("strapi"), {
@@ -15,6 +14,5 @@ renderAdmin(document.getElementById("strapi"), {
     graphql: graphql,
     i18n: i18N,
     "users-permissions": usersPermissions,
-    "vercel-deploy": vercelDeploy,
   },
 });
