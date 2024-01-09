0b1a74fab7221ac90828f133a21c3ed6d5e2531c
tankicagoneva <tankicaspasev@gmail.com>
2024-01-09 18:29:38 +0000

    refactor: server.js configuration

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,14 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  upload: {
+    // Customize this path based on your project structure
+    path: env("UPLOAD_PATH", "/public/uploads"),
   },
 });
